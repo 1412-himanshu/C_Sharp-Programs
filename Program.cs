497464067b141cdc5cef00442095df20438c8d18
Himanshu Hatwar <85329713+1412-himanshu@users.noreply.github.com>
2022-03-11 23:09:16 +0530

    First Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, This is my First C# Program");
+            Console.ReadLine();
+        }
+    }
+}
